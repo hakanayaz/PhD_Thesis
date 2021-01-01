092a5a554df80f22ae627c03aa883b4ae702a219
Denizhan Yigitbas <denizhanyigitbas@gmail.com>
2020-12-31 23:06:00 -0700

    found miniumu rotated degree

--- main.py
+++ main.py
@@ -5,6 +5,53 @@ from mpl_toolkits.mplot3d import Axes3D
 # from matplotlib.mlab import griddata
 from matplotlib import cm
 
+def Rz(theta):
+        return np.matrix([[math.cos(theta), -math.sin(theta), 0],
+                          [math.sin(theta), math.cos(theta), 0],
+                          [0, 0, 1]])
+
+def find_miniumum_degree(x_data, y_data, z_data):
+    x_difference = []
+    for t in range(180):
+        theta = -(t+1)
+        Rot_Matrix_Z = Rz(theta)
+        x_rot = []
+
+        for i in range(len(x_data)):
+            point = [[x_data[i]], [y_data[i]], [z_data[i]]]
+            # Utilize np.matmul operation for efficiency because matmul using C
+            rotated_mat = np.matmul(Rot_Matrix_Z, point)
+            arr_mat = np.squeeze(np.asarray(rotated_mat))
+            x_prime = arr_mat[0]
+            x_rot.append(x_prime)
+
+        x_diff = max(x_rot) - min(x_rot)
+        x_difference.append(x_diff)
+    
+    min_deg = x_difference.index(min(x_difference))
+    # store the minimum difference
+    min_dif = x_difference[min_deg]
+
+    return min_deg
+
+def rotate_data(x_data, y_data, z_data, theta):
+    Rot_Matrix_Z = Rz(theta)
+    x_rot = []
+    y_rot = []
+    z_rot = []
+    for i in range(len(x_data)):
+        point = [[x_data[i]], [y_data[i]], [z_data[i]]]
+
+        # Utilize np.matmul operation for efficiency because matmul using C
+        rotated_mat = np.matmul(Rot_Matrix_Z, point)
+        arr_mat = np.squeeze(np.asarray(rotated_mat))
+        x_prime = arr_mat[0]
+        y_prime = arr_mat[1]
+        z_prime = arr_mat[2]
+        x_rot.append(x_prime)
+        y_rot.append(y_prime)
+        z_rot.append(z_prime)
+    return x_rot, y_rot, z_rot
 
 def main():
     # Reading the .txt file
@@ -26,90 +73,12 @@ def main():
     ax.set_zlabel('Z Label', fontsize=10)
     ax.scatter3D(x, y, z, c=z, cmap=plt.cm.jet)
 
-    # [TOSEE] Plotting the data
-    # plt.show()
-
-    ############  ROTATING  #############
-    # Rotating the Point Cloud to be able to calculate the area
-
-    # Finding the [theta]
-    # theta = math.pi / 6 * -1
-
-    # Z Axis rotation definition
-    def Rz(theta):
-        return np.matrix([[math.cos(theta), -math.sin(theta), 0],
-                          [math.sin(theta), math.cos(theta), 0],
-                          [0, 0, 1]])
-
-    ##################################################################################
-
-    # Rot_Matrix_Z = Rz(theta)
-
-    # x_rot = []
-    # y_rot = []
-    # z_rot = []
-    # for i in range(len(x)):
-        # point = [[x[i]], [y[i]], [z[i]]]
-
-        # # Utilize np.matmul operation for efficiency because matmul using C
-        # rotated_mat = np.matmul(Rot_Matrix_Z, point)
-        # arr_mat = np.squeeze(np.asarray(rotated_mat))
-        # x_prime = arr_mat[0]
-        # y_prime = arr_mat[1]
-        # z_prime = arr_mat[2]
-        # x_rot.append(x_prime)
-        # y_rot.append(y_prime)
-        # z_rot.append(z_prime)
-
-    # # print("Original points X difference", max(x) - min(x))
-    # # print("Rotating points X  difference", max(x_rot) - min(x_rot))
-
-    # # Subplot helped me to graph together 122 - 121
-    # ax2 = fig.add_subplot(122, projection='3d')
-
-    # # Plot rotated points and sampled scatter result
-    # ax2.set_xlabel('X Label', fontsize=10)
-    # ax2.set_ylabel('Y Label', fontsize=10)
-    # ax2.set_zlabel('Z Label', fontsize=10)
-    # ax2.scatter3D(x_rot, y_rot, z_rot, c=z_rot, cmap=plt.cm.jet)
-
-    # # [TOSEE] Plotting the data
-    # plt.show()
-
-    ##################################################################################
-
-    # theta = math.pi / 6 * -1
-    for t in range(180):
-
-        # theta = (math.pi / (t+1)) * -1
-        theta = (t+1)
-        Rot_Matrix_Z = Rz(theta)
-
-        x_rot = []
-        y_rot = []
-        z_rot = []
-        x_difference = []
-        x_diff
-
-        for i in range(len(x)):
-            point = [[x[i]], [y[i]], [z[i]]]
-
-            # Utilize np.matmul operation for efficiency because matmul using C
-            rotated_mat = np.matmul(Rot_Matrix_Z, point)
-            arr_mat = np.squeeze(np.asarray(rotated_mat))
-            x_prime = arr_mat[0]
-            y_prime = arr_mat[1]
-            z_prime = arr_mat[2]
-            x_rot.append(x_prime)
-            y_rot.append(y_prime)
-            z_rot.append(z_prime)
-
-            x_diff = max(x_rot) - min(x_rot)
-        x_difference.append(x_diff)
-        print(len(x_difference))
-
-    # print("Original points X difference", max(x) - min(x))
-    # print("Rotating points X  difference", max(x_rot) - min(x_rot))
+    
+    min_degree = find_miniumum_degree(x,y,z)
+    rot_data = rotate_data(x,y,z,min_degree)
+    rotated_data_x = rot_data[0]
+    rotated_data_y = rot_data[1]
+    rotated_data_z = rot_data[2]
 
     # Subplot helped me to graph together 122 - 121
     ax2 = fig.add_subplot(122, projection='3d')
@@ -118,10 +87,10 @@ def main():
     ax2.set_xlabel('X Label', fontsize=10)
     ax2.set_ylabel('Y Label', fontsize=10)
     ax2.set_zlabel('Z Label', fontsize=10)
-    ax2.scatter3D(x_rot, y_rot, z_rot, c=z_rot, cmap=plt.cm.jet)
+    ax2.scatter3D(rotated_data_x, rotated_data_y, rotated_data_z, c=rotated_data_z, cmap=plt.cm.jet)
 
     # [TOSEE] Plotting the data
-    # plt.show()
+    plt.show()
 
 
 if __name__ == '__main__':
